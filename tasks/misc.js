5192cea8c92aa56b0e804c52fa5a1289bbc409bd
Alexander Anpleenko <vaeum@icloud.com>
2017-03-12 02:25:29 +0400

    Обновлено misc.js

--- tasks/misc.js
+++ tasks/misc.js
@@ -1,9 +1,8 @@
 import gulp from 'gulp';
-import browserSync from 'browser-sync';
-import { config, $ } from './config';
+import { config, $, bs } from './config';
 
 gulp.task('misc', () =>
   gulp.src(config.src.misc)
     .pipe(gulp.dest(config.dest.misc))
-    .on('end', browserSync.reload)
+    .on('end', bs.reload)
 );

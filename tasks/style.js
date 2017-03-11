a54ed855ed39b2f1933ae27a53738ddd8ae747c4
Alexander Anpleenko <vaeum@icloud.com>
2017-03-12 02:31:40 +0400

    Обновлено style.js

--- tasks/style.js
+++ tasks/style.js
@@ -4,22 +4,8 @@ import { config, $, bs } from './config';
 const reload = bs.reload;
 
 const source = [
-  './node_modules/bootstrap-only-css/lib/scaffolding.css',
-  './node_modules/bootstrap-only-css/lib/utilities.css',
-  './node_modules/bootstrap-only-css/lib/normalize.css',
-  './node_modules/bootstrap-only-css/lib/type.css',
-  './node_modules/bootstrap-only-css/lib/grid.css',
-  './node_modules/bootstrap-only-css/lib/responsive-utilities.css',
-  './node_modules/bootstrap-only-css/lib/responsive-embed.css',
-  './node_modules/bootstrap-only-css/lib/component-animations.css',
-  './node_modules/bootstrap-only-css/lib/modals.css',
   config.src.style,
-  './node_modules/css.modifiers/dist/modifiers.css',
 ];
-
-// const since = {
-//   since: gulp.lastRun('style')
-// }
 
 gulp.task('style', () =>
   gulp.src(source)

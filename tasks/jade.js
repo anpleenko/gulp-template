bec84772d2654f24b51abe7d1fa3957f6a29eb4c
Alexander Anpleenko <vaeum@yahoo.com>
2017-04-30 12:00:01 +0300

    change jade task

--- tasks/jade.js
+++ tasks/jade.js
@@ -2,16 +2,15 @@ import gulp from 'gulp';
 import { config, $, bs, errorHandler, isDev, isProd } from './config';
 import data from '../src/data';
 
-gulp.task('jade', () => {
-  return gulp.src([config.src.jade])
+gulp.task('jade', () =>
+  gulp.src([config.src.jade])
     .pipe($.jade({
       locals: { ...data, DEV: isDev, PROD: isProd },
-    }))
-      .on('error', errorHandler)
+    })).on('error', errorHandler)
 
     .pipe($.posthtml(config.POSTHTML_PROCESSORS))
     .pipe($.if(isDev, $.jsbeautifier(config.jsbeautifierConfig)))
 
     .pipe(gulp.dest(config.dest.app))
-    .on('end', bs.reload);
-});
+    .on('end', bs.reload)
+);

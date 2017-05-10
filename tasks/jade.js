a030309f6a442c33e6ec5932d8d4d1197584b1ec
Alexander Anpleenko <vaeum@yahoo.com>
2017-05-10 22:30:47 +0300

    fix jade task

--- tasks/jade.js
+++ tasks/jade.js
@@ -1,19 +1,17 @@
 import gulp from 'gulp';
 import { config, $, bs, notify, isDev, isProd } from './config';
 
-gulp.task('jade', () => {
-  const data = require('../src/data');
-
-  return gulp.src([config.src.jade])
+gulp.task('jade', () =>
+  gulp.src([config.src.jade])
     .pipe($.if(isDev, $.plumber({ errorHandler: notify('Jade error') })))
     .pipe($.debug())
     .pipe(
       $.jade({
-        locals: { ...data.default, DEV: isDev, PROD: isProd },
+        locals: { ...require('../src/data').default, DEV: isDev, PROD: isProd },
       }),
     )
     .pipe($.posthtml(config.POSTHTML_PROCESSORS))
     .pipe($.if(isDev, $.jsbeautifier(config.jsbeautifierConfig)))
     .pipe(gulp.dest(config.dest.app))
-    .on('end', bs.reload);
-});
+    .on('end', bs.reload),
+);

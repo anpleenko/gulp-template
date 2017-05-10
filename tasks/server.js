1b3eb5855c283ed7ccd744d0c6dd94164bc21da7
Alexander Anpleenko <vaeum@yahoo.com>
2017-05-10 21:59:23 +0300

    add callback in server task

--- tasks/server.js
+++ tasks/server.js
@@ -1,11 +1,13 @@
 import gulp from 'gulp';
 import { config, bs } from './config';
 
-gulp.task('server', () => {
+gulp.task('server', (cb) => {
   bs({
     server: {
       baseDir: config.dest.app,
     },
     open: false,
   });
+
+  cb();
 });

57e55e7faaffbac8ac4111bdc6d897ee08e4c0eb
Alexander Anpleenko <vaeum@yahoo.com>
2017-05-10 21:59:49 +0300

    change data in jade task

--- tasks/jade.js
+++ tasks/jade.js
@@ -1,18 +1,19 @@
 import gulp from 'gulp';
 import { config, $, bs, notify, isDev, isProd } from './config';
-import data from '../src/data';
 
-gulp.task('jade', () =>
-  gulp.src([config.src.jade])
+gulp.task('jade', () => {
+  const data = require('../src/data');
+
+  return gulp.src([config.src.jade])
     .pipe($.if(isDev, $.plumber({ errorHandler: notify('Jade error') })))
     .pipe($.debug())
     .pipe(
       $.jade({
-        locals: { ...data, DEV: isDev, PROD: isProd },
+        locals: { ...data.default, DEV: isDev, PROD: isProd },
       }),
     )
     .pipe($.posthtml(config.POSTHTML_PROCESSORS))
     .pipe($.if(isDev, $.jsbeautifier(config.jsbeautifierConfig)))
     .pipe(gulp.dest(config.dest.app))
-    .on('end', bs.reload),
-);
+    .on('end', bs.reload);
+});
